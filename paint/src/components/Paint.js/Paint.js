7e00e4d2be54960a93fd82cbb8554f36f860d69a
Baek <woonn97@gmail.com>
2023-12-16 15:04:00 +0900

    React 그림판 만들기 실습

---
+++ paint/src/components/Paint.js/Paint.js
@@ -0,0 +1,173 @@
+// 펜, 지우개, 채우기
+
+import { useCallback, useEffect, useRef, useState } from "react";
+import { PenPlugin } from "./plugins/pen";
+
+const calculateCoord = (e, canvas) => {
+  const rect = canvas.getBoundingClientRect();
+
+  return {
+    x: e.pageX - rect.left - window.scrollX,
+    y: e.pageY - rect.top - window.scrollY,
+  };
+};
+
+const Paint = ({
+  command = "pen",
+  color = "#000000",
+  lineWidth = 2,
+  width = 800,
+  height = 600,
+  plugins = [new PenPlugin()],
+  style,
+  className,
+}) => {
+  const [currentCommand, setCurrentCommand] = useState(command);
+  const [currentLineWidth, setCurrentLineWidth] = useState(lineWidth);
+  const [currentColor, setCurrentColor] = useState(color);
+  const [currentPlugins, setCurrentPlugins] = useState({});
+  const [drawing, setDrawing] = useState(false);
+
+  const canvasRef = useRef();
+
+  const scale = typeof window === "undefined" ? 1 : window.devicePixelRatio; // 안하면 흐릿하게 보임
+
+  const canvasDefaultStyle = {
+    userSelect: "none",
+    WebkitUserSelect: "none",
+  };
+
+  const canvasSizeStyle = {
+    width,
+    height,
+  };
+
+  useEffect(() => {
+    setCurrentLineWidth(lineWidth);
+  }, [lineWidth]);
+
+  useEffect(() => {
+    setCurrentCommand(command);
+  }, [command]);
+
+  useEffect(() => {
+    setCurrentColor(color);
+  }, [color]);
+
+  useEffect(() => {
+    plugins.forEach((plugin) => {
+      plugin.canvas = canvasRef.current;
+    });
+
+    setCurrentPlugins(
+      Object.assign(
+        {},
+        ...plugins.map((plugin) => ({
+          [plugin.name]: plugin,
+        }))
+      )
+    );
+  }, [canvasRef.current, plugins]);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    canvasRef.current.width = width * scale;
+    canvasRef.current.height = height * scale;
+
+    canvasRef.current.getContext("2d").scale(scale, scale);
+  }, [canvasRef.current, scale]);
+
+  const handleDrawStart = useCallback(
+    (e) => {
+      e.preventDefault();
+      const { x, y } = calculateCoord(e, canvasRef.current);
+
+      currentPlugins[currentCommand].draw({
+        x,
+        y,
+        width,
+        height,
+        scale,
+        lineWidth: currentLineWidth,
+        color: currentColor,
+        state: "draw-started",
+      });
+
+      setDrawing(true);
+    },
+    [canvasRef, currentCommand, currentColor, currentLineWidth, currentPlugins]
+  );
+
+  const handleDrawing = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!drawing) return;
+
+      const { x, y } = calculateCoord(e, canvasRef.current);
+
+      currentPlugins[currentCommand].draw({
+        x,
+        y,
+        width,
+        height,
+        scale,
+        lineWidth: currentLineWidth,
+        color: currentColor,
+        state: "drawing",
+      });
+
+      console.log("drawing");
+    },
+    [
+      canvasRef,
+      currentCommand,
+      currentColor,
+      currentLineWidth,
+      currentPlugins,
+      drawing,
+    ]
+  );
+
+  const handleDrawFinish = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!drawing) return;
+
+      const { x, y } = calculateCoord(e, canvasRef.current);
+
+      currentPlugins[currentCommand].draw({
+        x,
+        y,
+        width,
+        height,
+        scale,
+        lineWidth: currentLineWidth,
+        color: currentColor,
+        state: "draw-finished",
+      });
+      console.log("draw-finished");
+      setDrawing(false);
+    },
+    [
+      canvasRef,
+      currentCommand,
+      currentColor,
+      currentLineWidth,
+      currentPlugins,
+      drawing,
+    ]
+  );
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={handleDrawStart}
+      onMouseMove={handleDrawing}
+      onMouseUp={handleDrawFinish}
+      style={{ ...canvasDefaultStyle, ...canvasSizeStyle, ...style }}
+      className={className}
+    />
+  );
+};
+
+export default Paint;
---
+++ paint/src/components/Paint.js/plugins/eraser.js
@@ -0,0 +1,37 @@
+import { Plugin } from "./plugin";
+
+export class EraserPlugin extends Plugin {
+  oldX = -1;
+  oldY = -1;
+  constructor(initialValues) {
+    super({
+      ...initialValues,
+      name: "eraser",
+    });
+  }
+
+  draw(data) {
+    super.draw(data);
+    // state => draw-started, drawing, draw-finished
+    const { x, y, state } = data;
+    const context = this.canvas.getContext("2d");
+
+    if (this.oldX === -1) this.oldX = x;
+    if (this.oldY === -1) this.oldY = y;
+
+    context.globalCompositeOperation = "destination-out"; //그리는 영역이 지워짐
+
+    if (state === "draw-started" || state === "drawing") {
+      context.beginPath();
+      context.moveTo(this.oldX, this.oldY);
+      context.lineTo(x, y);
+      context.stroke();
+
+      this.oldX = x;
+      this.oldY = y;
+    } else {
+      this.oldX = -1;
+      this.oldY = -1;
+    }
+  }
+}
